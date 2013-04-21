110eef700b39a5552480a8b20c2d6db2d2562c01
Mygod Studio <mygodstudio@gmail.com>
2013-04-21 12:09:32 +0800

    1.4.24.0

--- Online/OfflineDownloader/Default.aspx
+++ Online/OfflineDownloader/Default.aspx
@@ -1,25 +1,19 @@
 ﻿<%@ Page Title="Mygod离线下载器" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Mygod.Website.ProductStore.Online.OfflineDownloader.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="Head" runat="server">
     <style type="text/css">
-        #link-box {
+        .link-box {
             width: 100%;
+            height: 300px;
         }
     </style>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $("#magic-button").click(function () {
-                var value = $("#link-box").val();
-                if (!isNullOrWhiteSpace(value)) location.href = "Start.aspx?URL=" + $.base64.encode(value.split("").reverse().join(""));
-            });
-        });
-    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Content" runat="server">
     <h2 class="center">Mygod离线下载器</h2>
     <div>迅雷会员一点也不拽，Mygod会员现在也可以获得离线下载的功能！它居然也可以当梯子使，不过……嘘！</div>
     <div>离线原理：先把东西从服务器上下载到Mygod，然后再传给你。</div>
     <div>把地址贴上来，然后按下那个神奇的按钮，在那之后你就可以看看服务器下载得怎么样了。（为了达到最佳速度推荐下载国外地址）</div>
-    <div><input type="text" id="link-box" /></div>
-    <div>&nbsp;</div>
-    <div class="center"><button type="button" id="magic-button">我就是那个神奇的按钮</button></div>
+    <div>离线下载器同样支持同时下载多个文件！在下面的窗口中粘贴上所有你要下载的地址然后点击那个神奇的按钮，稍等片刻后服务器就会返回下载链接。点击<a href="Fetch.aspx">这里</a>看看下载状态，当当前任务总数变为0/x时表示全部下载完成，复制以下的下载链接即可开始下载！</div>
+    <div>想要销毁任务？把你得到的地址中的Fetch改为Destroy然后打开即可！</div>
+    <div><asp:TextBox ID="LinkBox" runat="server" TextMode="MultiLine" CssClass="link-box"></asp:TextBox></div>
+    <div class="center"><asp:Button ID="SubmitButton" runat="server" Text="我就是那个神奇的按钮" OnClick="Submit" /></div>
 </asp:Content>
--- Online/OfflineDownloader/Default.aspx.cs
+++ Online/OfflineDownloader/Default.aspx.cs
@@ -1,9 +1,45 @@
-﻿using System.Web.UI;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using System.Web.UI;
+using System.Xml.Linq;
 
 namespace Mygod.Website.ProductStore.Online.OfflineDownloader
 {
     public partial class Default : Page
     {
-        // protected void Page_Load(object sender, EventArgs e) { }
+        private const string Prefix = "http://mygod.apphb.com/Online/OfflineDownloader/Fetch.aspx?Key=";
+        private string directoryPath;
+
+        protected void Submit(object sender, EventArgs e)
+        {
+            var result = new StringBuilder();
+            var lines = 0;
+            Directory.CreateDirectory(directoryPath = Server.MapPath("/Temp/OfflineDownloader/"));
+            foreach (var link in LinkBox.Text.Split(new[] { '\r', '\n' }).Where(link => !string.IsNullOrWhiteSpace(link)
+                        && !link.StartsWith(Prefix, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                lines++;
+                result.AppendLine(Prefix + DownloadTask(link));
+            }
+            if (lines == 1) Response.Redirect(result.ToString());
+            else LinkBox.Text = result.ToString();
+        }
+
+        private string DownloadTask(string url)
+        {
+            url = TFQR.Decode(TFQR.GetUrlType(url), url);
+            string md5 = FormsAuthentication.HashPasswordForStoringInConfigFile(url, "MD5"),
+                   path = Path.Combine(directoryPath, md5), xmlPath = path + ".xml";
+            if (!File.Exists(xmlPath))
+            {
+                new XDocument(new XElement("download", new XAttribute("url", url))).Save(xmlPath);
+                Process.Start(Server.MapPath("MygodOfflineDownloader.exe"), path);
+            }
+            return md5;
+        }
     }
 }
