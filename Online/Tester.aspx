c5937bd2ec2e319a5c5cd8224d00d22cb02734bb
Mygod Studio <mygodstudio@gmail.com>
2013-05-09 20:02:40 +0800

    1.5.28.4
    Add appfail reporting

--- Online/Tester.aspx
+++ Online/Tester.aspx
@@ -1,5 +1,4 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tester.aspx.cs" Inherits="Mygod.Website.ProductStore.Online.Tester" %>
-<%@ Import Namespace="System.IO" %>
 
 <!DOCTYPE html>
 
@@ -11,9 +10,8 @@
 <body>
     <form id="form1" runat="server">
     <div>
-        <%=new DirectoryInfo("D:").EnumerateFileSystemInfos().Aggregate(string.Empty, (c, s) => c + s.FullName + "<br />") %>
+    <% throw new Exception(); %>
     </div>
-        <div><asp:Button runat="server" ID="CreateGarbager" OnClick="CreateGarbage" Text="Test now" /></div>
     </form>
 </body>
 </html>
--- Online/Tester.aspx.cs
+++ Online/Tester.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -8,18 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Mygod.Website.ProductStore.Online
 {
-    public partial class Tester : Page
+    public partial class Tester : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void CreateGarbage(object sender, EventArgs e)
-        {
-            File.WriteAllText(@"D:\temp\vsmghgtz.v10\output\_PublishedWebsites\MygodProductStore\Online\WoGLeaderboard\Default.aspx.garbage", "Garbage");
-            Response.WriteFile(@"D:\temp\vsmghgtz.v10\output\_PublishedWebsites\MygodProductStore\Online\WoGLeaderboard\Default.aspx.garbage"); 
-            Response.WriteFile(@"D:\temp\vsmghgtz.v10\output\_PublishedWebsites\MygodProductStore\Online\WoGLeaderboard\Default.aspx");
         }
     }
 }
